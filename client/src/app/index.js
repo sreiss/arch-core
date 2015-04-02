bfe310e9bcb8f4516f7b6534c1d97699d7171558
trickyboy67 <brianjunk@hotmail.fr>
2015-04-02 16:48:54 +0200

    Init client core

---
+++ client/src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('client', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ngMaterial'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
