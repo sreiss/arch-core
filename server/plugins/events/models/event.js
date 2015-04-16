a2e9a674cb1bae4458cbc586d26e87018db73547
strictlyking <strictly.king@gmail.com>
2015-04-16 10:25:15 +0200

    recreated events models, routes and controllers

--- server/plugins/events/models/event.js
+++ server/plugins/events/models/event.js
@@ -1,35 +1,30 @@
 /**
- * Event model.
+ * Events plugin.
  *
  * @module arch/events
  * @copyright ArchTailors 2015
  */
 
-module.exports = function(Types)
-{
+
+/** Miscellaneous event */
+module.exports = function(Types) {
     return {
-        schema: {
-            name: {type: String, required: true},
-            type: {type :String, required: true},
-            begin: {type: Date, required: true},
-            end: {type :Date},
-            description: {type : String},
-            //data_reference: { type: Types.ObjectId, ref: 'kid_participant' },
-            //data_reference: String,
-            address_line_1: {type : String},
-            //address_line_2: String,
-            ville: {type:String},
-            //address_line_3: String,
-            country: {type:String},
-            zip: {type : Number},
-            guests: [{ evt_guest : {type: Types.ObjectId, ref: 'User'}}],
-            created: {type :Date}
-            //createdBy: {type: Types.ObjectId, ref: 'User'},
-            //modified: Date,
-            //modifiedBy: {type: Types.ObjectId, ref: 'User'},
-            //archived: Date,
-            //archivedBy: {type: Types.ObjectId, ref: 'User'},
-            //published: Number
-        }
+        schema:
+        {
+            dtstart: { type: Date, required: true},
+            dtend: {type: Date, required: true},
+            summary: {type: String, required: true},
+            location: {type: String, required: true},
+            description: {type: String, required: true},
+            transp: {type: String, required: true},
+            sequence: {type: String, required: true},
+            category: {type: String, required: true},
+            participants:
+                [{
+                    guest: {type: Types.ObjetId, ref: 'User', required: true},
+                    status: {type: String, required: true}
+                }]
+        },
+        priority: 1
     };
 };
