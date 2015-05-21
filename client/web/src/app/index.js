6c0eb47742b5bac9012b2ad0a757bf9d086ea29c
Brian <brianjunk@hotmail.fr>
2015-05-22 00:29:00 +0200

    Fix

--- client/web/src/app/index.js
+++ client/web/src/app/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('archCore', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngMaterial','ui.router', 'pascalprecht.translate', 'ui.bootstrap','ui.calendar','angular-md5', 'base64', 'angular-datepicker','mdDateTime'])
+angular.module('archCore', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngMaterial','ui.router', 'pascalprecht.translate', 'ui.bootstrap','ui.calendar','angular-md5', 'base64'])
   .config(function ($mdThemingProvider, $stateProvider, $urlRouterProvider, $translateProvider, i18nfrFRConstant, i18nenUSConstant)
 {
   $mdThemingProvider
