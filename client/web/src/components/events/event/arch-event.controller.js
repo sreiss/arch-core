578f54fbcafb4ec226adae9732c28d77e07e407b
Brian <brianjunk@hotmail.fr>
2015-05-21 20:47:49 +0200

    events

--- client/web/src/components/events/event/arch-event.controller.js
+++ client/web/src/components/events/event/arch-event.controller.js
@@ -18,6 +18,21 @@ angular.module('archCore')
     }
 
     $scope.event = new Event();
+    $scope.event.dtstart = moment();
+    $scope.event.dtend = moment();
+    $scope.event.summary = "";
+    $scope.event.description = "";
+    $scope.event.transp = "false";
+    $scope.event.sequence = "0";
+    $scope.event.participants = [{guest : null, status:""}];
+    $scope.event.course = "";
+    $scope.event.website = "";
+    $scope.event.information = "";
+    $scope.event.trainings = [{training:null}];
+    $scope.event.creator = null;
+    $scope.event.program = "";
+    $scope.event.runs = [{run:null}];
+    $scope.dtstart= {};
 
     if($stateParams.category){
       $scope.event.category = $stateParams.category;
@@ -26,38 +41,28 @@ angular.module('archCore')
       $scope.event.category = "event";
     }
     if($stateParams.date != null){
-      $scope.event.dtstart = moment($stateParams.date);
+      //$scope.event.dtstart = moment($stateParams.date);
     }
-
-    $scope.startDate = function(ev) {
-      $mdDialog.show({
-        controller: DialogController,
-        templateUrl: 'components/events/arch-date-time.html',
-        targetEvent: ev
-      })
-        .then(function(answer) {
-          $scope.event.dtstart = moment(answer);
-        });
-    };
-
-    $scope.endDate = function(ev) {
-      $mdDialog.show({
-        controller: DialogController,
-        templateUrl: 'components/events/arch-date-time.html',
-        targetEvent: ev
-      })
-        .then(function(answer) {
-          $scope.event.dtend = moment(answer);
-        });
+    $scope.day = 31;
+    $scope.month = 12;
+    $scope.hour = 23;
+    $scope.minute = 59;
+    var dataYears = [];
+    var currentYear = moment().year();
+    for (i = 0; i < 10; i++) {
+      dataYears.push(currentYear + i);
+    }
+    $scope.years = dataYears;
+    $scope.getNumber = function(num) {
+      return new Array(num);
     };
 
     $scope.addEvent = function()
     {
-
-      $scope.event.transp = "false";
-      $scope.event.sequence = "0.0";
-      $scope.event.category = $scope.event.category;
-      $scope.event.participants = [];
+      var arrayStart = $scope.dtstart.date.split("/");
+      //.minute($scope.dtsart.minute).heure($scope.dtstart.heure)
+      var tmpStart = moment().date(arrayStart[0]).month(arrayStart[1]).year(arrayStart[2]);
+      $scope.event.dtstart = tmpStart;
       console.log($scope.event);
       $scope.event.$save(function (result)
       {
@@ -68,7 +73,7 @@ angular.module('archCore')
               .position('top right')
               .hideDelay(3000)
           );
-          $state.go('events');
+          $state.go('calendar');
         }
         else
         {
