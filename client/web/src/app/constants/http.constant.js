e8105ac715918711da2c54a31e1ddd0e74506217
sreiss <simon.reiss57@gmail.com>
2015-05-22 08:38:10 +0200

    Prod urls

--- client/web/src/app/constants/http.constant.js
+++ client/web/src/app/constants/http.constant.js
@@ -2,10 +2,16 @@
 
 angular.module('archCore').constant('httpConstant',
 {
+  /* DEV
   casClientUrl: 'http://localhost:3010',
   casServerUrl: 'http://localhost:3020',
   coreClientUrl: 'http://localhost:3011',
   coreServerUrl: 'http://localhost:3021',
+  */
+  casClientUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3010',
+  casServerUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3020',
+  coreClientUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3011',
+  coreServerUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3021',
 
   signupType: {
     name: 'ASCPA',
@@ -14,5 +20,8 @@ angular.module('archCore').constant('httpConstant',
   },
 
   clientName: 'ARCH-ASCPA',
+  clientRedirectUri : 'http://acrobatt-vm11.psi.ad.unistra.fr:3011'
+  /*
   clientRedirectUri : 'http://localhost:3011'
+  */
 });
