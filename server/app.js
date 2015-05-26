9e453bc25cfc063dde53f8e9e70cbd75e43ff53e
Brian <brianjunk@hotmail.fr>
2015-05-26 14:19:36 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var broadway = require('broadway'),
     
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
