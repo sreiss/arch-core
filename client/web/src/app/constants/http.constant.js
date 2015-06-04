3a770c9743b99efa8ce85baf4e6763eeb2242f59
Brian <brianjunk@hotmail.fr>
2015-06-04 14:03:17 +0200

    udpate language

--- client/web/src/app/constants/http.constant.js
+++ client/web/src/app/constants/http.constant.js
@@ -7,9 +7,9 @@ angular.module('archCore').constant('httpConstant',
   casServerUrl: 'http://localhost:3020',
   coreClientUrl: 'http://localhost:3011',
   coreServerUrl: 'http://localhost:3021',
-
    */
-  /* PROD   */
+
+  /* PROD*/
   casClientUrl: 'http://acrobatt-vm12.psi.ad.unistra.fr:3020',
   casServerUrl: 'http://acrobatt-vm12.psi.ad.unistra.fr:3020',
   coreClientUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3021',
@@ -22,31 +22,9 @@ angular.module('archCore').constant('httpConstant',
   },
 
   clientName: 'ARCH-ASCPA',
-  /* */
-  clientRedirectUri : 'http://acrobatt-vm11.psi.ad.unistra.fr:3021',
 
-  //clientRedirectUri : 'http://localhost:3011',
-  test : {
-    "sProcessing":     "Traitement en cours...",
-    "sSearch":         "Rechercher&nbsp;:",
-    "sLengthMenu":     "Afficher _MENU_ &eacute;l&eacute;ments",
-    "sInfo":           "Affichage de l'&eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
-    "sInfoEmpty":      "Affichage de l'&eacute;l&eacute;ment 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
-    "sInfoFiltered":   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
-    "sInfoPostFix":    "",
-    "sLoadingRecords": "Chargement en cours...",
-    "sZeroRecords":    "Aucun &eacute;l&eacute;ment &agrave; afficher",
-    "sEmptyTable":     "Aucune donn&eacute;e disponible dans le tableau",
-    "oPaginate": {
-      "sFirst":      "Premier",
-      "sPrevious":   "Pr&eacute;c&eacute;dent",
-      "sNext":       "Suivant",
-      "sLast":       "Dernier"
-    },
-    "oAria": {
-      "sSortAscending":  ": activer pour trier la colonne par ordre croissant",
-      "sSortDescending": ": activer pour trier la colonne par ordre d&eacute;croissant"
-    }
-  }
-
+  clientRedirectUri : 'http://acrobatt-vm11.psi.ad.unistra.fr:3021'
+  /*
+  clientRedirectUri : 'http://localhost:3011',
+   */
 });
