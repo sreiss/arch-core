0adbd06cba93b958555bb1a15a164aece96ef30b
Brian <brianjunk@hotmail.fr>
2015-06-18 15:20:56 +0200

    commit media

--- server/plugins/galleries/controllers/media.js
+++ server/plugins/galleries/controllers/media.js
@@ -16,21 +16,20 @@ module.exports = function(mediaService)
     return {
         save: function(req, res, next)
         {
-            mediaService.save({
-                files: req.files,
-                body: req.body
-            })
-                .then(function(media)
-                {
-                    res.json({
-                        message: 'MEDIA_SAVED',
-                        value: media
-                    });
-                })
-                .catch(function(err)
-                {
-                    next(err);
+            req.body.name = "test";
+            mediaService.save({files: req.files, body: req.body})
+            .then(function(media)
+            {
+                res.json({
+                    message: 'MEDIA_SAVED',
+                    value: media
                 });
+
+            })
+            .catch(function(err)
+            {
+                next(err);
+            });
         }
     };
 
