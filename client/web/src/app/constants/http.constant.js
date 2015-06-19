6b28c767aed383f42ffb782bf7d2652021b4a712
Maxime RAUCH <maxime@rauch.fr>
2015-06-19 16:54:17 +0200

    Fix rule button.

--- client/web/src/app/constants/http.constant.js
+++ client/web/src/app/constants/http.constant.js
@@ -2,22 +2,23 @@
 
 angular.module('archCore').constant('httpConstant',
 {
-  /* DEV*/
+  /* DEV
   casClientUrl: 'http://localhost:3010',
   casServerUrl: 'http://localhost:3020',
   coreClientUrl: 'http://localhost:3011',
   coreServerUrl: 'http://localhost:3021',
   kidClientUrl: 'http://localhost:3014',
   kidServerUrl: 'http://localhost:3024',
+  clientRedirectUri : 'http://localhost:3011' */
 
-  /* PROD
+  /* PROD */
   casClientUrl: 'http://acrobatt-vm12.psi.ad.unistra.fr:3020',
   casServerUrl: 'http://acrobatt-vm12.psi.ad.unistra.fr:3020',
   coreClientUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3021',
   coreServerUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3021',
   kidServerUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3024',
   kidClientUrl: 'http://acrobatt-vm11.psi.ad.unistra.fr:3024',
-*/
+  clientRedirectUri : 'http://acrobatt-vm11.psi.ad.unistra.fr:3021',
 
   signupType: {
     name: 'ASCPA',
@@ -25,8 +26,5 @@ angular.module('archCore').constant('httpConstant',
     isPublic: 'true'
   },
 
-  clientName: 'ARCH-ASCPA',
-
-  //clientRedirectUri : 'http://acrobatt-vm11.psi.ad.unistra.fr:3021'
-  clientRedirectUri : 'http://localhost:3011'
+  clientName: 'ARCH-ASCPA'
 });
